2f4ae02486ad023df221a017ad4cec929b566f3e
SuddenH4X <NagaraHyuga@gmail.com>
2016-05-02 00:22:59 +0200

    moved amount calculation from customer to rental;

--- src/Customer.java
+++ src/Customer.java
@@ -42,22 +42,6 @@ class Customer {
     }
 
     private double amountFor(Rental aRental) {
-        double result = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (aRental.getDaysRented() > 2)
-                    result += (aRental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3)
-                    result += (aRental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
+        return aRental.getCharge();
     }
 }
